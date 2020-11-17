9861fddcafc428daa2220b440be5467e2edc3340
Mateo-Bc <mateobecettecura12345@gmail.com>
2020-11-17 20:28:25 -0300

    agrego models y estructuras en admin

--- config/FilmHub/admin.py
+++ config/FilmHub/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import *
 
+class combo_comida_Admin(admin.ModelAdmin):
+    exclude = ['monto_total']
+    list_display = ['comida', 'cant_comida', 'bebida', 'cant_bebida', 'precio_final']
+    list_display_links = ['comida', 'bebida', 'precio_final']
+
+class boleto_Admin(admin.ModelAdmin):
+    exclude = ['monto_total']
+    list_display = ['pelicula', 'asiento', 'precio_final']
+"""
+class paquete_Admin(admin.ModelAdmin):
+    exclude = ['precio_final']
+    list_display = ['precio_total']
+"""
 # Register your models here.
+
+admin.site.register(Funcion,)
+admin.site.register(Asiento,)
+admin.site.register(Pelicula,)
+admin.site.register(Comida,)
+admin.site.register(Bebida,)
+
+admin.site.register(Boleto, boleto_Admin)
+admin.site.register(Combo_Comida, combo_comida_Admin)
+
+#admin.site.register(Paquete, paquete_Admin)
--- config/FilmHub/models.py
+++ config/FilmHub/models.py
@@ -1,3 +1,102 @@
 from django.db import models
 
 # Create your models here.
+
+class Funcion(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    horario = models.CharField(max_length=5)
+
+    def __str__(self):
+        return (self.horario)
+
+class Asiento(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    fila = models.CharField(max_length=1)
+    butaca = models.IntegerField(default=0, blank=True, null=True)
+
+    def __str__(self):
+        return (self.fila + str(self.butaca))
+
+class Pelicula(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    nombre = models.CharField(max_length=25)
+    descripcion = models.CharField(max_length=100)
+    duracion = models.IntegerField(default=0, blank=True, null=True)
+    precio_base = models.IntegerField(default=0, blank=True, null=True)
+    funcion = models.ForeignKey(Funcion, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return(self.nombre + " " + "(" + str(self.duracion) + "min.)" + " - " + str(self.funcion))
+
+class Comida(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    nombre = models.CharField(max_length=25)
+    contenido_neto = models.IntegerField(default=0, blank=True, null=True)
+    precio_unidad = models.IntegerField(default=0, blank=True, null=True)
+
+    def __str__(self):
+        return (self.nombre + " (" + str(self.contenido_neto) + "g)" + " - " + "$" + str(self.precio_unidad))
+
+class Bebida(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    nombre = models.CharField(max_length=25)
+    contenido_neto = models.IntegerField(default=0, blank=True, null=True)
+    precio_unidad = models.IntegerField(default=0, blank=True, null=True)
+
+    def __str__(self):
+        return (self.nombre + " (" + str(self.contenido_neto) + "ml)" + " - " + "$" + str(self.precio_unidad))
+
+
+
+class Boleto(models.Model):
+    asiento = models.ForeignKey(Asiento, on_delete=models.CASCADE)
+    pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE)
+
+    monto_total = models.IntegerField(default=0, blank=True, null=True)
+
+    def precio_final(self):
+        valor = self.pelicula.precio_base
+        return '$' + str(valor)
+        self.monto_total = valor
+    monto_total.short_description = 'Monto Total'
+
+    def __str__(self):
+        return str(self.pelicula) + ' - Asiento: ' + str(self.asiento)
+
+class Combo_Comida(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    comida = models.ForeignKey(Comida, on_delete=models.CASCADE)
+    cant_comida = models.IntegerField(default=0, blank=True, null=True)
+    bebida = models.ForeignKey(Bebida, on_delete=models.CASCADE)
+    cant_bebida = models.IntegerField(default=0, blank=True, null=True)
+
+    monto_total = models.IntegerField(default=0, blank=True, null=True)
+
+    def precio_final(self):
+        valor = (self.comida.precio_unidad * self.cant_comida + self.bebida.precio_unidad * self.cant_bebida)
+        return '$' + str(valor)
+        self.monto_total = valor
+
+    def __str__(self):
+        return str(self.cant_comida) + " " + str(self.comida) + " y " + str(self.cant_bebida) + " " + str(self.bebida)
+
+
+
+"""
+class Paquete(models.Model):
+    boleto = models.ForeignKey(Boleto, on_delete=models.CASCADE)
+    combo_comida = models.ForeignKey(Combo_Comida, on_delete=models.CASCADE)
+    precio_final = models.IntegerField(default=0, blank=True, null=True)
+
+
+    def precio_total(self):
+        valor = (self.boleto.precio_final, self.combo_comida.precio_final)
+        return '$' + str(valor)
+        self.precio_final = valor
+"""
